a43d5ea05a550ffeb31821247b23886d01e9e684
Aldebaraan <aldecanedo97@gmail.com>
2024-10-11 23:07:06 -0400

    commit test

---
+++ ch3/the_simplest_add_on.py
@@ -0,0 +1,14 @@
+
+bl_info = {
+    "name " : "The simplest Add-on",
+    "author" : "Aldebaraan Canedo",
+    "version" : (1, 0),
+    "blender" : (3, 00, 0),
+    "description" : "A very simple add-on",
+    "warning" : "This is just for learning",
+    "category" : "Learning",
+}
+
+def register():
+    # this function is called when the add-on is disabled
+    pass
